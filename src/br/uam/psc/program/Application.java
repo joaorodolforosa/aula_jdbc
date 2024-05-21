af929f18fd8f925173eb2ccc4992a8994f414f29
Joao Rodolfo <joaorodolforosa@gmail.com>
2024-05-20 22:45:58 -0300

    Recupera os dados no banco utilizando Statement e ResultSet

--- src/br/uam/psc/program/Application.java
+++ src/br/uam/psc/program/Application.java
@@ -3,20 +3,29 @@ package br.uam.psc.program;
 import br.uam.psc.db.DB;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 /**
  *
  * @author Joao Rodolfo
  */
 public class Application {
-
+    
     public static void main(String[] args) throws SQLException {
 
         /*
         Utiliza o método getConnection de DB para iniciar uma conexão com o banco
          */
         Connection conexao = DB.getConnection();
-
+        // instancia um objeto Statement para as instruções SQL
+        Statement statement = conexao.createStatement();
+        // representa o conjunto de resultados da instrução SQL
+        ResultSet comanda = statement.executeQuery("SELECT * FROM comanda_tb");
+        while (comanda.next()) {
+            System.out.println(comanda.getInt("qtd") + ", " + comanda.getString("descricao") + ", " + comanda.getDouble("valor"));
+        }
+        
     }
-
+    
 }
